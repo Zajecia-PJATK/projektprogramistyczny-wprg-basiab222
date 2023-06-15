a9f8b6383717181b6097eeec206c36d947b5aac0
basia222 <basiab078@gmail.com>
2023-06-15 19:37:46 +0200

    Poprawione logowanie i rejestracja z popup window i walidacja danych

--- login_registration/login_layout.php
+++ login_registration/login_layout.php
@@ -6,7 +6,7 @@
     <link rel="stylesheet" href="login_registration.css">
 </head>
 <body>
-<form method="post" action="l<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
     <label>E-mail: </label>
     <input type="email" name="email" required/><br>
     <label>Hasło: </label>
@@ -15,9 +15,52 @@
 </form>
 <form method="post" action="registration_layout.php">
     <label>Nie masz jeszcze konta? </label><br>
-    <button type="submit" name="register" >Stwórz konto</button>
+    <button type="submit" name="register">Stwórz konto</button>
 </form>
 </body>
 </html>
 
+<?php
+require_once "../db_connection.php";
+require_once "../test_input.php";
+session_start();
 
+if (isset($_POST['login'])) {
+    if (isset($_POST['email']) && isset($_POST['password']) &&
+        filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+
+        $email = test_input($_POST['email']);
+        $password = test_input($_POST['password']);
+
+        $selectQueryCustomer = "SELECT * FROM Klient WHERE Email = '$email' AND Haslo = '$password';";
+        $selectQueryEmployee = "SELECT * FROM Pracownicy WHERE Email = '$email' AND Haslo = '$password';";
+
+        if (!empty($conn)) {
+            $resultCustomer = $conn->query($selectQueryCustomer);
+            $resultEmployee = $conn->query($selectQueryEmployee);
+        }
+
+        if ($resultCustomer->num_rows > 0) {
+            $_SESSION['email'] = $email;
+            header("Location: ../employee_app/menu_layout.php");
+            exit();
+        } else if ($resultEmployee->num_rows > 0) {
+            header("Location: ../employee_app/menu_layout.php");
+            exit();
+        } else {
+            echo '<script>
+        window.addEventListener("DOMContentLoaded", function() {
+            displayPopup("Zły adres e-mail lub hasło");
+        });
+    </script>';
+        }
+        $conn->close();
+    } else{
+        echo '<script>
+        window.addEventListener("DOMContentLoaded", function() {
+            displayPopup("Podaj poprawnie wszystkie dane!"); 
+        });
+    </script>';
+    }
+}
+?>
--- login_registration/registration_layout.php
+++ login_registration/registration_layout.php
@@ -6,11 +6,11 @@
     <link rel="stylesheet" href="login_registration.css">
 </head>
 <body>
-<form method="post" action="registration.php">
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
     <label>Imię: </label>
-    <input type="text" name="name" pattern="[a-zA-Z0-9]+" required/><br>
+    <input type="text" name="name" required/><br>
     <label>Nazwisko: </label>
-    <input type="text" name="surname" pattern="[a-zA-Z0-9]+" required/><br>
+    <input type="text" name="surname" required/><br>
     <label>E-mail: </label>
     <input type="email" name="email" required/><br>
     <label>Hasło: </label>
@@ -19,3 +19,48 @@
 </form>
 </body>
 </html>
+
+<?php
+require_once "../db_connection.php";
+require_once "../test_input.php";
+session_start();
+
+if (isset($_POST['register'])) {
+    if (isset($_POST['name']) && isset($_POST['surname']) && isset($_POST['email']) && isset($_POST['password'])) {
+        $name = test_input($_POST['name']);
+        $surname = test_input($_POST['surname']);
+        $email = test_input($_POST['email']);
+        $password = test_input($_POST['password']);
+
+        $checkQuery = "SELECT * FROM Klient WHERE Email = '$email'";
+
+        if (!empty($conn)) {
+            $checkResult = $conn->query($checkQuery);
+        }
+
+        if ($checkResult->num_rows > 0) {
+            echo '<script>
+        window.addEventListener("DOMContentLoaded", function() {
+            displayPopup("E-mail adres został już użyty");
+        });
+    </script>';
+        } else {
+            $insertQuery = "INSERT INTO Klient (Imie, Nazwisko, Email, Haslo) VALUES ('$name','$surname','$email','$password')";
+
+            if ($conn->query($insertQuery) === true) {
+                header("Location: login_layout.php");
+                exit();
+            } else {
+                echo 'Error: ' . $insertQuery . '<br>' . $conn->error;
+            }
+        }
+        $conn->close();
+    } else{
+        echo '<script>
+        window.addEventListener("DOMContentLoaded", function() {
+            displayPopup("Podaj poprawnie wszystkie dane!");
+        });
+    </script>';
+    }
+}
+?>
